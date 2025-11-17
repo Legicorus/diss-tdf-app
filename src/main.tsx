5b045bca9697077e0bc4234f5d7e057f6ce89de2
Leon Wolfert <leon@Asset-100195.local>
2025-11-17 09:19:33 +0100

    Deploy: Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// 👉 Vercel Analytics importieren
+import { Analytics } from '@vercel/analytics/react'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+
+    {/* 👉 Vercel Analytics aktivieren */}
+    <Analytics />
   </StrictMode>,
 )
+
