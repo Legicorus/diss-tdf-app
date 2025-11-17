4be98db8346e309298180e64d37880cfe48c1af7
Leon Wolfert <leon@Asset-100195.local>
2025-11-17 09:36:59 +0100

    Add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// 👉 Vercel Analytics importieren
+// Vercel Analytics
 import { Analytics } from '@vercel/analytics/react'
+
+// Vercel Speed Insights
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-
-    {/* 👉 Vercel Analytics aktivieren */}
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
-
